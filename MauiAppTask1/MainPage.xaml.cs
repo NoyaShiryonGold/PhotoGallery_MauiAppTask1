0d44077b8992689d2207947082a217e6fd07dca2
NoyaShiryonGold <1002113892@edu-darom.org.il>
2024-07-24 12:09:12 +0300

    final - c# control

--- MauiAppTask1/MainPage.xaml.cs
+++ MauiAppTask1/MainPage.xaml.cs
@@ -1,23 +1,78 @@
-﻿namespace MauiAppTask1
+﻿using static System.Net.Mime.MediaTypeNames;
+
+
+namespace MauiAppTask1
 {
     public partial class MainPage : ContentPage
     {
+        Button prevBtn, nextBtn;
+        Microsoft.Maui.Controls.Image imageHolder;
         int num =1;
         string[] images = ["a1.jpg", "a2.jpg", "a3.jpg", "a4.jpg", "a5.jpg"];
         public MainPage()
         {
             InitializeComponent();
-            PreBtn.IsEnabled = false;
+            LoadView();
+            nextBtn.IsEnabled = true;
+            prevBtn.IsEnabled = false;
         }
+
+        private void LoadView()
+        {
+            #region Labels
+            Label header = new Label();
+            header.Text = "Image APP";
+            header.Style = (Style)Microsoft.Maui.Controls.Application.Current.Resources["Headline"];
+            header.FontFamily = "Pacifico";
+            SemanticProperties.SetHeadingLevel(header, SemanticHeadingLevel.Level2);
+            Label description = new Label();
+            description.Text = "Click the arrows to change image";
+            description.Style = (Style)Microsoft.Maui.Controls.Application.Current.Resources["SubHeadline"];
+            description.FontFamily = "EduAU";
+            SemanticProperties.SetHeadingLevel(description, SemanticHeadingLevel.Level4);            
+            #endregion
+            #region Buttons
+            prevBtn = new Button();
+            prevBtn.Text = "\uf182";
+            prevBtn.Style = (Style)Microsoft.Maui.Controls.Application.Current.Resources["IconButton"];
+            SemanticProperties.SetHint(prevBtn, "previous image");
+            prevBtn.Clicked += OnArrowClicked;
+            nextBtn = new Button();
+            nextBtn.Text = "\uf181;";
+            nextBtn.Style = (Style)Microsoft.Maui.Controls.Application.Current.Resources["IconButton"];
+            SemanticProperties.SetHint(nextBtn, "next image");
+            nextBtn.Clicked += OnArrowClicked;
+            #endregion
+            #region Image&Border
+            imageHolder = new Microsoft.Maui.Controls.Image();
+            imageHolder.Source = ImageSource.FromFile("logo.jpg");
+            imageHolder.HeightRequest = 250;
+            imageHolder.Aspect = Aspect.AspectFit;
+            SemanticProperties.SetDescription(imageHolder, "image view");
+            Border border=new Border();
+            border.Stroke = Microsoft.Maui.Controls.Brush.CadetBlue;
+            border.StrokeThickness = 10;
+            border.HorizontalOptions = LayoutOptions.Center;
+            border.Content=imageHolder;
+            #endregion
+            #region Layout
+            MainLayout.Children.Add(description);
+            MainLayout.Children.Add(header);
+            MainLayout.Children.Add(prevBtn);
+            MainLayout.Children.Add(border);
+            MainLayout.Children.Add(nextBtn);
+            #endregion
+        }
+
 
         private void OnArrowClicked(object sender, EventArgs e)
         {
-            if (sender==PreBtn)
+            if (sender==prevBtn)
             {               
                 if (num > 0)
                 {
                     num--;
-                    ImageHolder.Source = images[num];
+                    imageHolder.Source = images[num];
                 }
             }
             else
@@ -25,11 +80,11 @@
                 if (num <images.Length-1)
                 {
                     num++;
-                    ImageHolder.Source = images[num];
+                    imageHolder.Source = images[num];
                 }
             }
-            PreBtn.IsEnabled = num > 0; 
-            NextBtn.IsEnabled = num < images.Length-1;
+            prevBtn.IsEnabled = num > 0; 
+            nextBtn.IsEnabled = num < images.Length-1;
             //SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
